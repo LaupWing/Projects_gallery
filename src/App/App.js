10145d4900ec93bdc1c0b43aa71151e0b3ac8180
LaupWing <loc_strict@hotmail.com>
2020-07-24 13:46:47 +0200

    added router and switch

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from './App.module.css'
-import SearchBar from '../components/SearchBar/SearchBar'
+import {Route, Switch} from 'react-router-dom'
 import Nav from '../components/Nav/Nav'
+import Projects from './views/Projects/Projects'
+import Home from './views/Home/Home'
 
 function App() {
     return (
         <div className={styles.App}>
             <Nav/>
-            <SearchBar/>
+            <main>
+                <Switch>
+                    <Route path="/projects" component={Projects}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
+            </main>
         </div>
     );
 }
