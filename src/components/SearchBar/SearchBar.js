b63ead51145f7b390294c51c4ee6b780b86349e5
LaupWing <loc_strict@hotmail.com>
2020-07-24 17:52:32 +0200

    succesfully injected the icons

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import styles from './SearchBar.module.css'
 
 const SearchBar = ()=>{
-    const icons = ['Django', 'Electron', 'Firebase', 'Javascript', 'Laravel', 'Mongodb'];
+    const icons = [
+        'Django', 
+        'Electron', 
+        'Firebase', 
+        'Javascript', 
+        'Laravel', 
+        'Mongodb'
+    ]
+    
     return(
         <div className={styles.SearchBar}>
             <input type="text" placeholder="Search for a project..."/>
-            {icons.map(icon=>(
-                 React.lazy(() => import(`../Icons/${icon}`))
-            ))}
+            <div></div>
+            <Suspense fallback={<div>Loading...</div>}>
+                {icons.map((icon, i)=>{
+                    const Component = React.lazy(() =>
+                        import(`../Icons/${icon}/${icon}`)
+                    );
+                    return <Component key={i}/>;
+                })}
+            </Suspense>
         </div>
     );
 }
