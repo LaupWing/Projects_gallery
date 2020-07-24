1b8327515cc320608c9c413c2575372ddb69fd2e
LaupWing <loc_strict@hotmail.com>
2020-07-24 13:54:42 +0200

    working links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import styles from './Nav.module.css'
-
+import NavItem from './NavItem/NavItem'
 
 const Nav = (props)=>{
     return (
         <nav className={styles.Nav}>
             <li>Logo</li>
-            <li>Home</li>
-            <li>Projects</li>
+            <NavItem
+                link="/"
+                exact
+            >
+                Home
+            </NavItem>
+            <NavItem
+                link="/projects"
+            >
+                Projects
+            </NavItem>
         </nav>
     )
 }
---
+++ src/components/Nav/NavItem/NavItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import styles from './NavItem.module.css'
+
+const NavItem = props => (
+    <li>
+        <NavLink
+            to={props.link}
+            activeClassName={styles.active}
+            exact={props.exact}
+        >
+            {props.children}
+        </NavLink>
+    </li>
+)
+
+export default NavItem
