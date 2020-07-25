6cbe5beaa16ebcefdfb07bdfb32a29fda4148377
LaupWing <loc_strict@hotmail.com>
2020-07-25 20:44:10 +0200

    transiont doesnt work properly

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './SearchBar.module.css'
 import MadeBy from './MadeBy/MadeBy'
 import Status from './Status/Status'
 import SortBy from './SortBy/SortBy'
 import MoreFilters from './MoreFilters/MoreFilters'
+import CSSTransition from 'react-transition-group/CSSTransition'
 
 const SearchBar = ()=>{
+    const [showMore, setShowMore] = useState(false)
     return(
         <div className={styles.SearchBar}>
             <input type="text" placeholder="Search for a project..."/>
             <MadeBy/>
             <Status/>
             <SortBy/>
-            <MoreFilters/>
-            <button>Show more</button>
+            <CSSTransition
+                mountOnEnter={true}
+                unmountOnExit={true}
+                in={showMore}
+                timeout={1000}
+                classNames='scale-up'
+            >
+                <MoreFilters/>
+            </CSSTransition>
+            <button onClick={()=>setShowMore(!showMore)}>Show more</button>
         </div>
     );
 }
