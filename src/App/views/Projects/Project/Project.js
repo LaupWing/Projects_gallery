d1c7e95cb896281ef4f4de5b126903f146a9177b
LaupWing <loc_strict@hotmail.com>
2020-07-27 22:43:10 +0200

    done for today

--- src/App/views/Projects/Project/Project.js
+++ src/App/views/Projects/Project/Project.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './Project.module.css';
+import icons_loader from '../../../../components/Icons/icons_loader'; 
 
 const Project = ({project}) => {
-    const image = require(`../../../../assets/projects_images/${project.image}`)
+    const image = require(`../../../../assets/projects_images/${project.image}`);
+    const MadeBy = icons_loader[project.madeBy];
+    const Status = icons_loader[project.status];
     return (
         <div className={styles.Project}>
             <h2>{project.title}</h2>
@@ -10,9 +13,11 @@ const Project = ({project}) => {
             <div className={styles.info}>
                 <div className={styles.field}>
                     <h3>Made By</h3>
+                    <MadeBy/>
                 </div>
                 <div className={styles.field}>
                     <h3>Status</h3>
+                    <Status/>
                 </div>
             </div>
         </div>
