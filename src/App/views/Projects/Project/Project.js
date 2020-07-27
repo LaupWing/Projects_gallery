c2ab291b5cc081283982dae120757f5fa4662703
LaupWing <loc_strict@hotmail.com>
2020-07-27 22:27:02 +0200

    lil udate

--- src/App/views/Projects/Project/Project.js
+++ src/App/views/Projects/Project/Project.js
@@ -6,7 +6,15 @@ const Project = ({project}) => {
     return (
         <div className={styles.Project}>
             <h2>{project.title}</h2>
-            <img src={image}/>
+            <img alt={image} src={image}/>
+            <div className={styles.info}>
+                <div className={styles.field}>
+                    <h3>Made By</h3>
+                </div>
+                <div className={styles.field}>
+                    <h3>Status</h3>
+                </div>
+            </div>
         </div>
     );
 }
