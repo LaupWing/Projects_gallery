995c75f6b6651cf0c66a22fd21ecf6b94db1401c
LaupWing <loc_strict@hotmail.com>
2020-07-28 12:30:38 +0200

    added overlay

--- src/App/views/Projects/Project/Project.js
+++ src/App/views/Projects/Project/Project.js
@@ -4,27 +4,31 @@ import icons_loader from '../../../../components/Icons/icons_loader';
 
 const Project = ({project}) => {
     const image = require(`../../../../assets/projects_images/${project.image}`);
-    // const MadeBy = icons_loader[project.madeBy];
-    // const Status = icons_loader[project.status];
+    const MadeBy = icons_loader[project.madeBy];
+    const Status = icons_loader[project.status];
 
     return (
         <div className={styles.Project}>
             <h2>{project.title}</h2>
-            <img alt={image} src={image}/>
+            <div className={styles.imageContainer}>
+                <img alt={image} src={image}/>
+                <div className={styles.info}>
+                    <div className={styles.field}>
+                        <h3>Made By</h3>
+                        <MadeBy/>
+                    </div>
+                    <div className={styles.field}>
+                        <h3>Status</h3>
+                        <Status/>
+                    </div>
+                </div>
+            </div>
             <div className={styles.stack}>
                 <h3>Stack</h3>
-                {project.stack.map(x=>{
+                {project.stack.map((x,i)=>{
                     const Icon = icons_loader[x]
-                    return <Icon/>
+                    return <Icon key={i}/>
                 })}
-                {/* <div className={styles.field}>
-                    <h3>Made By</h3>
-                    <MadeBy/>
-                </div>
-                <div className={styles.field}>
-                    <h3>Status</h3>
-                    <Status/>
-                </div> */}
             </div>
         </div>
     );
