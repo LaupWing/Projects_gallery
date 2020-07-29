160243c1e578c9e3cf322d6a272143be18c60014
LaupWing <loc_strict@hotmail.com>
2020-07-29 19:39:09 +0200

    sorting works

--- src/App/views/Projects/Projects.js
+++ src/App/views/Projects/Projects.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 import Project from './Project/Project';
 import styles from './Projects.module.css';
 import Message from '../../../components/Message/Message';
+import moment from 'moment';
 
 const Projects = ({projects, filters, sortBy})=>{
     const [message, setMessage] = useState(false);
@@ -12,8 +13,14 @@ const Projects = ({projects, filters, sortBy})=>{
         .filter(x=>!x.stack.find(y=>filters.includes(y)))
         .filter(x=>!filters.includes(x.status)&&!filters.includes(x.madeBy))
         .sort((a,b)=>{
+            const timestampA = moment(a.createdAt, "D MMMM YYYY").format('X')
+            const timestampB = moment(b.createdAt, "D MMMM YYYY").format('X')
             if(sortBy === 'favorite'){
                 return a.rank < b.rank ? -1 : b.rank < a.rank ? 1 : 0 
+            }else if(sortBy === 'up'){
+                return timestampA < timestampB ? -1 : timestampB < timestampA ? 1 : 0
+            }else if(sortBy === 'down'){
+                return timestampA > timestampB ? -1 : timestampB > timestampA ? 1 : 0
             }
         })
     return(
