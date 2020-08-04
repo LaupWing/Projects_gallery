3e26d4306f1fcd712fe09334415f39ebaf314d60
LaupWing <loc_strict@hotmail.com>
2020-08-04 17:17:49 +0200

    build

---
+++ build/precache-manifest.05ffa2479109a54e77c2513fe891d362.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb942adaee43c1c8d07545ec3bd7e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8126232fc3cf4dc1aaff",
+    "url": "/static/css/main.5b67c930.chunk.css"
+  },
+  {
+    "revision": "d2f80cd14ff6a5f2861b",
+    "url": "/static/js/2.a33986af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a33986af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8126232fc3cf4dc1aaff",
+    "url": "/static/js/main.75375b89.chunk.js"
+  },
+  {
+    "revision": "37dd09abdd54588561e4",
+    "url": "/static/js/runtime-main.e2743f95.js"
+  },
+  {
+    "revision": "574b60cb2f231ad32ff73229f1d543c8",
+    "url": "/static/media/code_snippets.574b60cb.PNG"
+  },
+  {
+    "revision": "d7efe85727bdb596e03c93016025b82a",
+    "url": "/static/media/hayden.d7efe857.PNG"
+  },
+  {
+    "revision": "0809bcbe5bf39bc11ec96a4b0c23c20f",
+    "url": "/static/media/jones.0809bcbe.PNG"
+  },
+  {
+    "revision": "8e56faef92a950132892d226da1043cd",
+    "url": "/static/media/react_mini_projects.8e56faef.PNG"
+  },
+  {
+    "revision": "2e964fbf6c2c75d982b12d452f8492cb",
+    "url": "/static/media/react_type_test.2e964fbf.PNG"
+  },
+  {
+    "revision": "59c6023eca69a6f0ac648b73298a8614",
+    "url": "/static/media/skills.59c6023e.PNG"
+  },
+  {
+    "revision": "ed0205669a246e8e08eba260d6417a4d",
+    "url": "/static/media/sphere.ed020566.PNG"
+  },
+  {
+    "revision": "079587dd16a2f4ed5b0c654cc958b743",
+    "url": "/static/media/tosti.079587dd.PNG"
+  },
+  {
+    "revision": "ab2b03ddff9ba3c11af491ba506c29ad",
+    "url": "/static/media/welsh.ab2b03dd.PNG"
+  },
+  {
+    "revision": "5dd191e9e089416a9492f8c27e4d710e",
+    "url": "/static/media/windows95.5dd191e9.PNG"
+  }
+]);
