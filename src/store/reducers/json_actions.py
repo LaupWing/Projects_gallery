9792c0fafadd889d42732f87baa899811f387d4e
LaupWing <loc_strict@hotmail.com>
2020-08-07 18:46:49 +0200

    qq

--- src/store/reducers/json_actions.py
+++ src/store/reducers/json_actions.py
@@ -1,22 +1,32 @@
 import json
 import os
+import sys
 from pprint import pprint
 
 json_file = next((x for x in os.listdir() if '.json' in x), None)
 
 class json_actions:
-    def __init__(self):
+    def __init__(self, prop):
         self.data = []
         self.json_file = next((x for x in os.listdir() if '.json' in x), None)
+        self.prop = prop
         if not self.json_file:
             print('no json file found')
-        else:
-            print('json file found')
+        self.modify_json()
+
+    def get_prop(self, item):
+        return item.get(self.prop)
+
     def modify_json(self):
         with open(os.path.join(os.getcwd(),json_file)) as f:
             self.data = json.load(f)
-            self.data.sort(key=get_rank)
+            if self.prop not in self.data[0]:
+                print('passed prop is not available')
+                return
+            self.data.sort(key=self.get_prop)
 
+test = json_actions(sys.argv[1])
+# print(sys.argv[1])
 # def get_rank(item):
 #     return item.get('rank')
 
