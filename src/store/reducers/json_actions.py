633f172024a59dab223d9e546ceece256fe6d34f
LaupWing <loc_strict@hotmail.com>
2020-08-07 18:22:23 +0200

    test

--- src/store/reducers/json_actions.py
+++ src/store/reducers/json_actions.py
@@ -2,15 +2,26 @@ import json
 import os
 from pprint import pprint
 
-print(os.listdir())
-
 json_file = next((x for x in os.listdir() if '.json' in x), None)
 
-def get_rank(item):
-    return item.get('rank')
+class json_actions:
+    def __init__(self):
+        self.data = []
+        self.json_file = next((x for x in os.listdir() if '.json' in x), None)
+        if not self.json_file:
+            print('no json file found')
+        else:
+            print('json file found')
+    def modify_json(self):
+        with open(os.path.join(os.getcwd(),json_file)) as f:
+            self.data = json.load(f)
+            self.data.sort(key=get_rank)
 
-with open(os.path.join(os.getcwd(),json_file)) as f:
-    data = json.load(f)
-    data.sort(key=get_rank)
-    with open(os.path.join(os.getcwd(),json_file), 'w') as f2:
-        json.dump(data, f2)
+# def get_rank(item):
+#     return item.get('rank')
+
+# with open(os.path.join(os.getcwd(),json_file)) as f:
+#     data = json.load(f)
+#     data.sort(key=get_rank)
+#     with open(os.path.join(os.getcwd(),json_file), 'w') as f2:
+#         json.dump(data, f2)
