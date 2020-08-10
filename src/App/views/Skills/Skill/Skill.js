d65e0ba6dd2a4cac30e2cbc361a4812094b96312
LaupWing <loc_strict@hotmail.com>
2020-08-10 20:20:55 +0200

    show unfinsished

--- src/App/views/Skills/Skill/Skill.js
+++ src/App/views/Skills/Skill/Skill.js
@@ -2,12 +2,32 @@ import React from 'react'
 import icons_loader from '../../../../components/Icons/icons_loader'
 import styles from './Skill.module.css'
 
-const Skill = ({skill}) => {
+const Skill = ({skill, projects, max}) => {
     const Icon = icons_loader[skill.icon]
+    const calcWidth = (status)=>{
+        const finished = projects.filter(x=>x.status ===status) 
+        return 100 - ((max - finished.length) * (100/max))  
+    }
     return (
         <div className={styles.skill}>
             <div className={styles.progress_container}>
                 <Icon/>
+                <div className={styles.progress}>
+                    <div 
+                        className={styles.progress_bar_finished}
+                        style={{
+                            width: `${calcWidth('finished')}%`
+                        }}
+                    >
+                    </div>
+                    <div 
+                        className={styles.progress_bar_unfinished}
+                        style={{
+                            width: `${calcWidth('unfinished')}%`
+                        }}
+                    >
+                    </div>
+                </div>
             </div>
             <button>Show projects</button>
         </div>
--- src/App/views/Skills/Skills.js
+++ src/App/views/Skills/Skills.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import skill_list from './skill_list'
 import Skill from './Skill/Skill'
+import {connect} from 'react-redux'
 
-const Skills = () => {
+const Skills = ({projects}) => {
+    const skills_projects = skill_list
+        .map(x=>{
+            return projects.filter(y=>y.stack.includes(x.icon))
+        })
+        .map(y=>y.length)
+    const max_projects = Math.max.apply(Math,skills_projects)
+    
     return (
         <>
-            {skill_list.map(skill=> <Skill skill={skill}/>)}
+            {skill_list.map((skill, i)=> 
+                <Skill 
+                    key={i} 
+                    skill={skill}
+                    projects={projects.filter(x=>x.stack.includes(skill.icon))}
+                    max={max_projects}
+                />
+            )}
         </>
     )
 }
+const mapStateToProps = (state)=>{
+    return{
+        projects: state.projects
+    }
+}
 
-export default Skills
+
+export default connect(mapStateToProps)(Skills)
