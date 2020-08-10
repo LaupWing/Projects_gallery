f240dd6de48e817c4f3b569e9246980d40c071f7
LaupWing <loc_strict@hotmail.com>
2020-08-10 20:35:43 +0200

    tooltip

--- src/App/views/Skills/Skill/Skill.js
+++ src/App/views/Skills/Skill/Skill.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import icons_loader from '../../../../components/Icons/icons_loader'
 import styles from './Skill.module.css'
+import Tooltip from '../../../../components/Tooltip/Tooltip'
 
 const Skill = ({skill, projects, max}) => {
     const Icon = icons_loader[skill.icon]
+    const [showTooltip, setShowTooltip] = useState(false)
     const calcWidth = (status)=>{
         const finished = projects.filter(x=>x.status ===status) 
         return 100 - ((max - finished.length) * (100/max))  
     }
     return (
-        <div className={styles.skill}>
+        <div 
+            className={styles.skill}
+            onMouseOver={()=>setShowTooltip(true)}
+            onMouseOut={()=>setShowTooltip(false)}
+        >
+            {showTooltip && <Tooltip message={'test'}/>}
             <div className={styles.progress_container}>
                 <Icon/>
                 <div className={styles.progress}>
