a1f5645e2d928ad1572488b60f3e53e38d2c5668
LaupWing <loc_strict@hotmail.com>
2020-08-17 20:24:24 +0200

    home page

--- src/App/views/Home/Home.js
+++ src/App/views/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Logo from '../../../components/Logo/Logo'
+import styles from './Home.module.css'
 
 const Home = ()=>{
     return(
-        <h2>Home</h2>
+    <>
+        <h2 className={styles.name}><Logo/><span>Project</span> <span>Manager</span></h2>
+    </>
     )
 }
 
