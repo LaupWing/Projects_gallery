eac70d11a2ede88bed5d6501b5e18b4dc1b979f9
LaupWing <loc_strict@hotmail.com>
2020-08-17 22:38:46 +0200

    sections

--- src/App/views/Home/Home.js
+++ src/App/views/Home/Home.js
@@ -5,8 +5,10 @@ import Logo from '../../../components/Logo/Logo'
 const Home = ()=>{
     return(
     <>
-        <h2 className={styles.name}><Logo/> <p><span>Project</span> <span>Manager</span></p></h2>
-        <p className={styles.content}>Welcome on my portfolio website called Project Manager. On this website you can primarily find all the projects I have made throughout my coding career.</p>
+        <section className={styles.block}>
+            <h2 className={styles.name}><Logo/> <p><span>Project</span> <span>Manager</span></p></h2>
+            <p className={styles.content}>Welcome on my portfolio website called Project Manager. On this website you can primarily find all the projects I have made throughout my coding career.</p>
+        </section>
         <p className={styles.content}>Who am I?I am currently a student at the Hoge School van Amsterdam and I live in Heiloo. Im very passioned about code and programming in general. Interested on which programming languages and frameworks I have used? Click on the the skills link above!</p>
     </>
     )
