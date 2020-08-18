dc09ecec0d443cf545487b4ec0dd719fa10004af
LaupWing <loc_strict@hotmail.com>
2020-08-18 15:34:42 +0200

    with color container

--- src/App/views/Home/Home.js
+++ src/App/views/Home/Home.js
@@ -15,7 +15,9 @@ const Home = ()=>{
                 <path  d="M593.5,1080.5s9.79-332,337.54-355.36c5.21-.38,10.44-.45,15.66-.37,27.27.41,159.4-9.41,339.11-218.83a395.29,395.29,0,0,0,43.29-61.34c29.86-52.3,123.12-181.13,344.36-247.49a99.82,99.82,0,0,0,64.73-60.22C1759.73,80,1810.94-7.67,1920.5.5v1080Z"/>
             </svg> */}
         </section>
-        <p className={styles.content}>Who am I?I am currently a student at the Hoge School van Amsterdam and I live in Heiloo. Im very passioned about code and programming in general. Interested on which programming languages and frameworks I have used? Click on the the skills link above!</p>
+        <section className={`${styles.block} ${styles.with_color}`}>
+            <p className={styles.content}>Who am I?I am currently a student at the Hoge School van Amsterdam and I live in Heiloo. Im very passioned about code and programming in general. Interested on which programming languages and frameworks I have used? Click on the the skills link above!</p>
+        </section>
     </>
     )
 }
