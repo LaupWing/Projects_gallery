66536f682a8c4a960d401112e8f5e4c0c3ea7384
LaupWing <loc_strict@hotmail.com>
2020-08-19 11:32:46 +0200

    base icon

---
+++ src/App/views/Home/Icon/Icon.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import Tooltip from '../../../../components/Tooltip/Tooltip';
+import styles from './Icon.module.css';
+import icons_loader from '../../../../components/Icons/icons_loader';
+
+const Icon = ({iconObj})=>{
+    const {icon, message} = iconObj;
+    const Component = icons_loader[icon];
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    return (
+        <div 
+            className={styles.Icon}
+            onMouseOver={()=>setShowTooltip(true)}
+            onMouseOut={()=>setShowTooltip(false)}
+        >
+            {showTooltip && <Tooltip
+                message={message}
+            />}
+            <Component/>
+        </div>
+    );
+}
+
+
+export default Icon;
